a953264be4a59467d5a59039f2f0bcf36f70d6d7
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-11-17 19:06:58 +0100

    Addes MusicXML samples from MuseScore sources (sha 871c8ce2002e8b3c198f532fdb4fbcce7914f951)

---
+++ musicxml-samples/musescore-samples/get-samples.sh
@@ -0,0 +1,5 @@
+musescore_source_path='...'
+mkdir -p sample{1,2,3}
+cp ${musescore_source_path}/mtest/musicxml/io/*xml sample1
+cp ${musescore_source_path}/test/musicxml/*xml     sample2
+cp ${musescore_source_path}/test/lilyXml/*xml      sample3
